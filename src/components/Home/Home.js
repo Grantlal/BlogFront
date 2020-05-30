8459bddc79ab7f2d905ffc834c01be6e101b9986
Grant <granttlalonde@gmail.com>
2020-05-30 11:10:32 -0700

    Made some minor changes to Home style and content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,11 +26,15 @@ export default function render()
 {
         return (
           <div>
-            <h2>Home Page</h2>
             <div class="container">
                 <div class="jumbotron" id="Jumbo">
                     <h2>Welcome to Xander Zone</h2>      
                 </div>     
+            </div>
+            <div>
+                <p>
+                    This section will be where I explain what the point of this website is
+                </p>
             </div>
          </div>
         );
