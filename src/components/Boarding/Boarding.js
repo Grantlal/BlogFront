eaf156a9a2547d83b899f4bdc5da89f80daa56c1
Grant <granttlalonde@gmail.com>
2020-06-08 11:56:02 -0700

    Updated boarding content

--- src/components/Boarding/Boarding.js
+++ src/components/Boarding/Boarding.js
@@ -5,11 +5,20 @@ import './Boarding.css';
 export default function render()
 {
         return (
-          <div>
-          <div>
+          <div class="container">
+          <div class="container">
               <div class="jumbotron" id="Jumbo">
-                  <h1>Boarding</h1>      
               </div>     
+          </div>
+          <div id="MainText">
+            <p>
+              Yo welcome to the what could be the best section here! The first thing I wanted to talk about here is a brief overview of my skateboarding story. 
+              If we start out that way once I start going into more specific content hopefully you the reader can better understand where I'm coming from. 
+              Without any more fluff let's move onto to the good stuff.
+            </p>
+            <p>
+              So I started getting into skateboarding when I was in my early pre-teen years. I'm imagining around 6-7th grade is when the group really started. 
+            </p>
           </div>
         </div>
         );
