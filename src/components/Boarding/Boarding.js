2d8d9d7e8bc3e312a7aa7a682a1cae804dbc9e8e
Grant <granttlalonde@gmail.com>
2020-06-09 14:56:06 -0700

    Added some Boarding content

--- src/components/Boarding/Boarding.js
+++ src/components/Boarding/Boarding.js
@@ -18,6 +18,18 @@ export default function render()
             </p>
             <p>
               So I started getting into skateboarding when I was in my early pre-teen years. I'm imagining around 6-7th grade is when the group really started. 
+              We obviously had all played Tony Hawk games and what not, but now we wanted to get into skateboarding for real. So I remember I was using my
+              brother's old board, and he didn't want me to really play with it at all. So my friend and I started searching for boards on craigslist. We found
+              an old zero board and went for it. Then I got an old pair of the venture salt and peppers from another buddy. I don't recall the wheels or bearings
+              at all though so anyone from that time feel free to chime in... 
+            </p>
+            <p>
+              Anyways the gang started to skateboard a lot more. This started with our driveways and the streets where we learned some basic tricks like ollies and
+              shuv-ies. We actually also bought a small round-flat rail and absolutely horrendous kicker. When we found out none of us could hit those things we ended
+              up building planky. Planky was made from some old two by fours and the metal end of a bed frame. This legend sits a solid like 10 inches maybe from the
+              ground, and has a few screws that stick out. The beauty of this obstacle was though, that we could hit it. So I remember having the guys over and 
+              we would have a little driveway session with planky and the other obstacles. After we were tuckered out we'd go back inside and pound some Dr Pepper 
+              probably and paly videogames until three in the morning. We didn't go to the skatepark really much at all yet, but that's coming. 
             </p>
           </div>
         </div>
