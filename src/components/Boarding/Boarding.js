95faf7e3d0311f9b75169fe654eeeac6b35e1208
Grant <granttlalonde@gmail.com>
2020-06-09 15:20:50 -0700

    Finished rough draft of Boarding

--- src/components/Boarding/Boarding.js
+++ src/components/Boarding/Boarding.js
@@ -31,6 +31,29 @@ export default function render()
               we would have a little driveway session with planky and the other obstacles. After we were tuckered out we'd go back inside and pound some Dr Pepper 
               probably and paly videogames until three in the morning. We didn't go to the skatepark really much at all yet, but that's coming. 
             </p>
+            <p>
+              I now had become a freshman in high school and that's when I started going to the skatepark a lot more. Meeting some friends in school who also were 
+              interested in skateboarding we needed a place to meet and that was the skatepark. During this time I skated the most I ever have in my life. Basically 
+              everyday after school I would go right to the skatepark and "shred" for hours. I was starting to get some box tricks, I could hit rails occassionally, 
+              and I loved playing S.K.A.T.E. For those of you that don't know what S.K.A.T.E. is, imagine a game of horse for basketball. Instead of trick shots, 
+              you try to match tricks. I like to think I was alright at the game, but since I still can't flip a kickflip well I really wasn't. Anyways most of my 
+              skating ended there pretty quickly as I ended up breaking my wrist fairly badly in 2012 which lead to me having a plate and a few screws put in. This 
+              made ended up making me try something new and I had always wanted to play hockey so I shot for stars and did that. 
+            </p>
+            <p>
+              Anyways queue up college and how one of my old skate buddies became my roommate, and two other buddies lived in the same building on the same floor as us
+              it was only a matter of time until we ended up skating again. Which we did quite a few times at the Plaza. The obstacles here were all much bigger and 
+              honestly didn't allow me to grow much as a skater. There were some fun things to hit like a little three stair, and a manny pad. After another year or two 
+              skating here infrequently I basically stopped again once I found snowboarding. I think that story is for another time, but ultimately I stopped skating 
+              altogether for the next few years until recently. 
+            </p>
+            <p>
+              Fast forward to me about to move across the country and I started to get interested in skating again. I blame "The Nine Club" as they run quite the
+              entertaining podcast. I start skating a bit more for fun here and there, but there's a global pandemic going on right now that frowns upon me going to 
+              the skateparks. My last board was probably about 5 years old and just got a massive pressure crack so I bought a new deck and shoes. I'm really excited to
+              continue learning and becoming a better skateboarder as time goes on. I think for the next entry in the skateboarding side I'm going to talk about the new deck
+              and how it's a lot different from what I'm used to. 
+            </p>
           </div>
         </div>
         );
