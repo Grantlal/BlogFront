681386168c5e6584ac6940dc1b1b2faf7cc28511
Grant <granttlalonde@gmail.com>
2020-06-16 15:51:08 -0700

    updated Recipes

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -12,7 +12,14 @@ export default function render()
             </div>
             <div class="container" id="MainText">
               <p>
-                toaster
+                Hi there! Welcome to where I'm going to keep a bunch of knowledge on my cooking habits. For the first entry I don't think I will go over any particular
+                recipe, and instead I will give you an idea what kinds of recipes to expect from me. Throughout this first post I will go through some of my favorite 
+                cuisine types, tools I use, and techniques I frequent. 
+              </p>
+              <p>
+                To start I wanted to go over what kinds of dishes you might expect out of me. I'm no Bobby Flay, and that means I don't have "specialty" cuisine type I 
+                gravitate towards. I'm very much like my peers and I like being able to go get chimichangas, fried chicken, italian, and basically everything else anytime
+                I want. 
               </p>
             </div>
           </div>
