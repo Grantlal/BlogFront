8872a3f4a30883a770e4512f699faa65e5e14547
Grant <granttlalonde@gmail.com>
2020-07-02 12:55:55 -0700

    Updating recipes content

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -14,12 +14,22 @@ export default function render()
               <p>
                 Hi there! Welcome to where I'm going to keep a bunch of knowledge on my cooking habits. For the first entry I don't think I will go over any particular
                 recipe, and instead I will give you an idea what kinds of recipes to expect from me. Throughout this first post I will go through some of my favorite 
-                cuisine types, tools I use, and techniques I frequent. 
+                tools I use in the kitchen, and whatever miscellaneous extras I feel we need to cover. 
               </p>
               <p>
-                To start I wanted to go over what kinds of dishes you might expect out of me. I'm no Bobby Flay, and that means I don't have "specialty" cuisine type I 
-                gravitate towards. I'm very much like my peers and I like being able to go get chimichangas, fried chicken, italian, and basically everything else anytime
-                I want. 
+                To start I think that the tools you use in the kitchen are highly important. It's similar to the idea that how can build something of quality from something
+                that is not of quality. Anyways I think the most important tools I use most frequently are some sort of nice pan (We can go so into depth on this is absurd), 
+                a half decent knife set, quite a large stockpot, and some random other objects maybe. Anyways the normally the big debate I see on cooking discussion is about
+                the knife. By all means if you are passionate for knives and their construction I totally understand. A high grade cutlery set is something I really wish I 
+                had. Instead I think the method of cooking is what will really help. For this you need good pans and to take of them. I currently am on a big cast iron binge
+                of cooking as that thing is magical. Like the crust you can develop with a cast iron is just wonderful. I definitely recommend getting one if you don't have one, 
+                but unfortunately it's not as totally versatile as a stainless steel or non-stick for that matter. I also frequent the non-stick, I know I shouldn't, but I'm poor. 
+                Someone send me an all clad. Anyways moving on the knife discussion again I've used a cheap set of knives that were really sharp and that worked fantastic. My one
+                Chef's knife from a inexpesive set (Like $40?) lasted me nearly a year with absolutely no problems. I occassionally tried to keep it sharp and it was perfect. I
+                only had a few small oopsies through the year too using it. This leads up to the stockpot. Another tool that I think is such an underrated piece of kitchen equipment. 
+                With a single large stockpot you can make any amount of pasta, soups, broths, CHILI, seriously you name it. The stainless steel stockpot I had was perfect to maybe
+                brown some meat and vegetables, then use the fond it made to make deliciuous soups and chili. Is chili a soup? Let me know how you feel. I think it's a grey area and 
+                I probably lean more towards it not being a soup. Distractions aside the large stainless stockpot is amazing and you should invest the money into one. Now 
               </p>
             </div>
           </div>
